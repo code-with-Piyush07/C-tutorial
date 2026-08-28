76934a2d46639a512d072106dfc2940c8fc3c3ce
Piyush <singh2214piyush@gmail.com>
2026-08-28 23:00:35 +0530

    first c code

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+   printf("hello world");
+    //int number = 5;
+     //char star = '*';
+     //int age = 24;
+    // age = 26;
+    // printf(age);
+    return 0;
+}
