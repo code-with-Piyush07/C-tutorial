a7aa150ce9b7f69ab6fe114f2b97ef4db485af41
Piyush <singh2214piyush@gmail.com>
2026-08-30 12:02:23 +0530

    area add power

--- hello.c
+++ hello.c
@@ -1,15 +1,38 @@
-#include <stdio.h>
+//area of square 
+/*#include<stdio.h>
 int main(){
-
-   
-    int number = 5;
-     char star = '*';
-     int age = 24;
-     float pi = 3.14;
-     printf("number value is %d",number);
-     printf("star value is %c",star);
-     printf("age value is %d",age);
-     printf("value of pi is %f",pi );
-
+    int a;
+    printf("enter side value");
+    scanf("%d",&a);
+    printf("area is : %d",a*a);
     return 0;
 }
+addition of 2 numbers
+ #include<stdio.h>
+ int main(){
+    int a,b;
+    printf("enter a");
+    scanf("%d",&a);
+
+    printf("enter b");
+    scanf("%d",&b);
+
+    printf("sum is %d",a+b);
+    return 0;
+ }
+   //area of square 
+   #include<stdio.h>
+   int main(){
+    float r;
+    printf("enter r ");
+    scanf("%f",&r);
+    printf("area of square is :%f",3.14*(r*r));
+   }*/
+   #include<stdio.h>
+   #include<math.h>
+   int main(){
+    int b,c;
+    b=c=1;
+    int a = pow(b,c);
+    printf("%d",a);
+   }
