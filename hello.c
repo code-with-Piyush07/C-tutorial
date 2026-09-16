603f8e018be6f5e8938c4927dd121427df2a4dd2
Piyush <singh2214piyush@gmail.com>
2026-09-16 10:59:37 +0530

    switch nd ternary

--- hello.c
+++ hello.c
@@ -1,47 +1,54 @@
 /*#include<stdio.h>
-#include<math.h>
 int main(){
-    int a = (int) 1.99999;
-    printf("%d",a);
+    int age;
+    printf("Enter age");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("adult \n");
+    }
+    else if(age>=13 && age<18){
+        printf("teenager \n");
+    }
+    else{
+        printf("child");
+    }
     return 0;
-
 }
 #include<stdio.h>
-#include<math.h>
 int main(){
-    int a = 9+4*10;
-    printf("%d",a);
-    return 0;
-
-}
-
-#include<stdio.h>
-#include<math.h>
+    int age;
+    printf("enter age : ");
+    scanf("%d",&age);
+    age>=18?printf("adult"):printf("child");
+    return 0;}*/
+    //switch statement 
+ #include<stdio.h>
 int main(){
-    int a = 4*3/6*2;
-    printf("%d",a);
-    return 0;
-
-}
-#include<stdio.h>
-#include<math.h>
-int main(){
-    int x;
-    printf("Enter a number" " : ");
-    scanf("%d",&x);
-    printf("%d",x%2==0);
-}
-  
-#include<stdio.h>
-int main(){
-    int itsMonday = 0;
-    int itsraining = 1;
-    printf("%d \n",itsraining|| itsMonday);
-}*/
-#include<stdio.h>
-int main(){
-    int x;
-    printf("enter value x ");
-    scanf("%d",&x);
-    printf(" %d " ,(x>9)&&(x<100));
-}
+    int day;
+    printf("enter day(1-7) : ");
+    scanf("%d",&day);
+    switch(day){
+        case 1 : 
+        printf("monday");
+        break;
+        case 2 : 
+        printf("tuesday");
+        break;
+        case 3 : 
+        printf("wednesday");
+        break;
+        case 4 : 
+        printf("thursday");
+        break;
+        case 5 : 
+        printf("friday");
+        break;
+        case 6 : 
+        printf("saturday");
+        break;
+        case 7 : 
+        printf("sunday");
+        break;
+        default: printf("enter valid");
+    }
+    return 0;}
